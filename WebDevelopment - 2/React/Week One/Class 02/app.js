42059fe7c7a0cc435a2d7d7d1368645087d72cc7
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-06-12 16:29:54 +0500

    Variable

--- WebDevelopment - 2/React/Week One/Class 02/app.js
+++ WebDevelopment - 2/React/Week One/Class 02/app.js
@@ -1,3 +1,67 @@
 // Connect JavaScript
 
 // alert("Hello Students");
+
+/ Data Types
+
+// Numbers
+// 123456789;
+
+// Boolean
+// true;
+// false;
+
+// String
+// "abcdef";
+// "abc123";
+// "1213456";
+// "true";
+// "false";
+
+// Errors and code are shown in Console only (f12)
+
+// Variable Create (One or more times usage of value in js)
+
+// myFullName (Camel Case)
+// my_full_name (Snake Case)
+
+// Variable (String)
+
+// var fullName = "Adil Ahmed";
+
+// alert(fullName);
+// alert(fullName);
+// alert(fullName);
+// alert(fullName);
+
+// Update Variable
+
+// var fullName = "Ali";
+
+// alert(fullName);
+
+// var fullName = "Adil Ahmed";
+
+// alert(fullName);
+
+// Undefined Variable
+
+// var fullName;
+
+// Variable (Numbers)
+
+// var num1 = 10;
+
+// alert(num1);
+
+// alert(num1 + 10);
+
+// var num1 = 10;
+// var num2 = 10;
+
+// alert(num1 + num2);
+
+// Print Value in Console
+// var Name = "Adil Ahmed";
+
+// console.log(Name);
