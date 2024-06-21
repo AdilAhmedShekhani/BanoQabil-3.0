fb394a0843d2195d4733e2b8d26181d04de2eef8
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-06-21 10:53:26 +0500

    Array of Objects

---
+++ WebDevelopment - 2/React/Week Three/Class 04/Marksheet/app.js
@@ -0,0 +1,19 @@
+const totalMarks = 200;
+// Array of Objects
+const students = [
+  {
+    name: "Ali",
+    scienceMarks: 90,
+    englishMarks: 70,
+  },
+  {
+    name: "Huzaifa",
+    scienceMarks: 10,
+    englishMarks: 40,
+  },
+  {
+    name: "Asim",
+    scienceMarks: 40,
+    englishMarks: 60,
+  },
+];
