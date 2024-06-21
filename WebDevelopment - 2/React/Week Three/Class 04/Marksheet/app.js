1e09d94f0e5ab7185f48bf2ccee558b4c0b8aa10
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-06-21 10:54:30 +0500

    Turnery Operator

--- WebDevelopment - 2/React/Week Three/Class 04/Marksheet/app.js
+++ WebDevelopment - 2/React/Week Three/Class 04/Marksheet/app.js
@@ -17,3 +17,21 @@ const students = [
     englishMarks: 60,
   },
 ];
+
+function calculateMarks(student) {
+  const total = student.scienceMarks + student.englishMarks;
+  const percentage = (total / totalMarks) * 100;
+  return { percentage, total };
+}
+
+for (let index = 0; index < students.length; index++) {
+  const result = calculateMarks(students[index]);
+  console.log("-------------------------------------");
+  console.log("-------------------------------------");
+  console.log("Name: " + students[index].name);
+  console.log("Science: " + students[index].scienceMarks);
+  console.log("English: " + students[index].englishMarks);
+  console.log("Total Marks: " + result.total);
+  console.log("Percentage: " + result.percentage);
+  result.percentage >= 40 ? console.log("PASSED") : console.log("FAILED"); // Turnery Operators
+}
