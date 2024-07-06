c3b1910513239448ba8fd61f7cde49be993acda0
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-07-06 12:41:14 +0500

    Content Updated

--- WebDevelopment - 2/React/Week Five/Class 08/first project/src/App.jsx
+++ WebDevelopment - 2/React/Week Five/Class 08/first project/src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-     <h1>Hello Students </h1>
+      {/* {alert("Hello")} */}
+      <h1 style={{ color: "blue", backgroundColor: "#000", padding: 30 }}>
+        Hello Students{" "}
+      </h1>
+      <br />
+      <h1 className="head2">Hello Students 1</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
