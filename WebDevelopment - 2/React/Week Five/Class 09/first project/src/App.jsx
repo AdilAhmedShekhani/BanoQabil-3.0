b0c798b660f8a59be1926a81ea29e9b1231bbfb0
AdilAhmedShekhani <adilaziz2009@live.com>
2024-07-07 00:11:12 +0500

    Class9 Created

---
+++ WebDevelopment - 2/React/Week Five/Class 09/first project/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import CAR from "./assets/car.jpg";
+import "./App.css";
+import Header from "./Components/header";
+import Footer from "./Components/footer";
+
+// function Header() {
+//   return (
+//     <div>
+//       <h1>Header</h1>
+//     </div>
+//   );
+// }
+
+// function Footer() {
+//   return (
+//     <div>
+//       <h1>Footer</h1>
+//     </div>
+//   );
+// }
+
+function App() {
+  return (
+    <>
+      <Header />
+      {/* {alert("Hello")} */}
+      <h1 style={{ color: "blue", backgroundColor: "#000", padding: 30 }}>
+        Hello Students{" "}
+      </h1>
+      <br />
+      <h1 className="head2">Hello Students 1</h1>
+      <img src={CAR} alt="" />
+      <img
+        src="https://hips.hearstapps.com/hmg-prod/images/dw-burnett-pcoty22-8260-1671143390.jpg?crop=0.668xw:1.00xh;0.184xw,0&resize=640:*"
+        alt=""
+      />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ WebDevelopment - 2/React/Week Five/Class 09/first project/src/Components/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div>
+      <h1>Footer</h1>
+    </div>
+  );
+}
+
+export default Footer;
