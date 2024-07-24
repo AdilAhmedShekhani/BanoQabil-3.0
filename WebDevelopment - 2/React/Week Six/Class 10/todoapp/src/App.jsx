fcd90d866bae52d9aac297ff56ba6914deaf1836
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-07-24 10:23:22 +0500

    Array of Object Rendering

--- WebDevelopment - 2/React/Week Six/Class 10/todoapp/src/App.jsx
+++ WebDevelopment - 2/React/Week Six/Class 10/todoapp/src/App.jsx
@@ -1,15 +1,26 @@
 function App() {
   //Maping of Array (List Rendering)
-  const arr = ["One", "Two", "Three"];
+  // const arr = ["One", "Two", "Three"];
 
+  //Maping of object (List Rendering)
+  // const arr = [{ text: "One" }, { text: "Two" }, { text: "Three" }];
   return (
     <>
       {/* {arr} */}
-      <ul>
+
+      {/* <ul>
         {arr.map((v, i) => (
           <li key={i}>{v}</li>
         ))}
-      </ul>
+      </ul> */}
+
+      {/* <ul>
+        {arr.map((v, i) => (
+          <li key={i}>{v.text}</li>
+        ))}
+      </ul> */}
+
+
     </>
   );
 }
