a59e2fc3a11ba12c8365c377671ee6cfc208e6cf
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-08-03 10:35:28 +0500

    Add AntDesign

--- WebDevelopment - 2/React/Week Seven/Class 12/todoapp/src/App.jsx
+++ WebDevelopment - 2/React/Week Seven/Class 12/todoapp/src/App.jsx
@@ -1,11 +1,21 @@
-
 import Button from "./components/Button/button";
-
+// Using AntDesign
+import { QRCode } from "antd";
+import Login from "./components/Login/login";
 function App() {
   return (
     <>
       <div>
         <Button />
+        {/* Using Ant Design */}
+        {/* <QRCode color="Blue" value={"https://banoqabil.pk/"} /> */}
+        <div className="container">
+          <div className="row mt-5 justify-content-center">
+            <div className="col-12 col-sm-12 col-md-8 col-lg-6">
+              <Login />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
