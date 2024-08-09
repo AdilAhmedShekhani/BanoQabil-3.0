31f2716955e834b3ce18f35751e2327c412789ed
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-08-09 09:11:17 +0500

    Form Created

---
+++ WebDevelopment - 2/React/Week Eight/Class 14/login/src/pages/Login.jsx
@@ -0,0 +1,54 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+function Login() {
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-6">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              <div id="emailHelp" className="form-text">
+                We'll never share your email with anyone else.
+              </div>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
+            <div className="mb-3 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+              />
+              <label className="form-check-label" htmlFor="exampleCheck1">
+                Check me out
+              </label>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
