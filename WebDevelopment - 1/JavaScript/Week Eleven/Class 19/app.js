bcd27b5e5970f09218f407b951c45ab370784763
AdilAhmedShekhani <adilaziz2009@live.com>
2024-08-20 22:39:27 +0500

    Loops /Flag/ Break

--- WebDevelopment - 1/JavaScript/Week Eleven/Class 19/app.js
+++ WebDevelopment - 1/JavaScript/Week Eleven/Class 19/app.js
@@ -1,1 +1,64 @@
 alert("Hello Students")
+
+// Create loops
+
+// For Loop
+
+// for(statement, condition, increment){
+//     //Block
+// }
+
+//statement condition increment
+// for (var i = 1; i <= 100; i++) {
+//   console.log("Loop==>", i);
+// }
+
+// var text = "Hello Students";
+
+// for (var i = 1; i <= 5; i++) {
+//   console.log("Loop==>", i, text);
+// }
+
+// var cities = ["karachi", "hyderabad", "Sukkur", "Multan", "Islamabad"];
+// for (var i = 0; i < cities.length ; i++) {
+//   console.log("Cities==>", cities[i]);
+// }
+
+// Table Print
+// var table = 2; //static value
+// for (var i = 1; i <= 10; i++) {
+//   console.log("Table ==>", table + "X" + i + "=" + table * i);
+// }
+
+// Flag
+
+// var cities = ["karachi", "hyderabad", "Sukkur", "Multan", "Islamabad"];
+// var flag = false;
+// for (var i = 0; i < cities.length; i++) {
+//   if (cities[i] == "Multan") {
+//     flag = true;
+//   }
+// }
+
+// if (flag) {
+//   console.log("Found");
+// } else {
+//   console.log("Not Found");
+// }
+
+// Break
+
+// for (var i = 0; i < 10; i++) {
+//   console.log(i);
+//   if (i === 4) {
+//     break;
+//   }
+// }
+
+// Palindrome
+
+// civic;
+// madam;
+// radar;
+// level;
+// refer;
