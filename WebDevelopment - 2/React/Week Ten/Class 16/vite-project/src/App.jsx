1efe22b30db5cf4970c39a24a8e043fdf718a716
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-08-23 09:40:23 +0500

    Use Callback

--- WebDevelopment - 2/React/Week Ten/Class 16/vite-project/src/App.jsx
+++ WebDevelopment - 2/React/Week Ten/Class 16/vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -7,9 +7,13 @@ import Child from "./components/Child/Child";
 function App() {
   const [count, setCount] = useState(0);
 
+  const getData = useCallback(()=>{
+    return ["one","two"]
+  },[])
+
   return (
     <>
-      <Child />
+      <Child getData={getData} />
       <h1>{count}</h1>
       <button onClick={() => setCount((oldValue) => oldValue + 1)}>
         Increase
