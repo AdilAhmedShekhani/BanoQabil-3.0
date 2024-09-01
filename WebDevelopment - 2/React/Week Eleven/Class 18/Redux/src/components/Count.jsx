3d8ef79081155ed3a71fe84a704398bd8a3118da
AdilAhmedShekhani <adilaziz2009@live.com>
2024-09-01 15:38:08 +0500

    Create Counter Using Redux

---
+++ WebDevelopment - 2/React/Week Eleven/Class 18/Redux/src/components/Count.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import { useSelector } from "react-redux";
+
+const Counter = () => {
+    const count = useSelector(state => state)
+    return (
+        <div>
+            <h2>{count}</h2>
+        </div>
+    )
+}
+
+export default Counter;
---
+++ WebDevelopment - 2/React/Week Eleven/Class 18/Redux/src/redux/store.js
@@ -0,0 +1,11 @@
+import { createStore } from "redux";
+
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT': return state + 1;
+        case 'DECREMENT': return state - 1;
+        default: return state;
+    }
+}
+
+export const store = createStore(reducer);
