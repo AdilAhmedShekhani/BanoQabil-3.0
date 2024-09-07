51f3fbd7a81f599a9a363ce9285c3accd2a7ce2d
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-09-07 11:47:28 +0500

    Text Expansion

--- WebDevelopment - 1/JavaScript/Week Twelve/Class 21/app.js
+++ WebDevelopment - 1/JavaScript/Week Twelve/Class 21/app.js
@@ -20,15 +20,6 @@
 //   age >= 18 ? "You are eligible to vote" : "You are not eligible to vote";
 // console.log(message);
 
-// Example handling events in the DOM
-// var element = document.getElementById("my-element");
-// element.addEventListener("click", function () {
-//   console.log("Element clicked!");
-// });
-
-// element.addEventListener("keydown", function (event) {
-//   console.log("Key pressed: " + event.key);
-// });
 
 // SetTimeOut (For Delay)
 
@@ -56,46 +47,3 @@
 //     link.innerHTML = "See more";
 //   }
 // }
-
-// Promise
-
-let promise = new Promise(function (resolve, reject) {
-    const x = "adil@banoqabil.com";
-    const y = "adil@banoqabil.com";
-    if (x === y) {
-      resolve();
-    } else {
-      reject();
-    }
-  });
-  
-  promise
-    .then(function () {
-      console.log("Success, You are a Login");
-    })
-    .catch(function () {
-      console.log("Some error has occurred");
-    });
-  
-  // Callback
-  
-  // The add() function is
-  // called with arguments a, b
-  // and callback, callback
-  // will be executed just
-  // after ending of add() function
-  function add(a, b, callback) {
-    console.log(`The sum of ${a} 
-    and ${b} is ${a + b}`);
-    callback();
-  }
-  
-  // The disp() function is called just
-  // after the ending of add() function
-  function disp() {
-    console.log(`This must be printed
-     after addition`);
-  }
-  
-  // Calling add() function
-  add(5, 6, disp);
