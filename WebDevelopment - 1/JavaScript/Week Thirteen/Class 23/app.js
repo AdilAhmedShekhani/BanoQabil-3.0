6255740f1ae99f0a650c8e395f8d3e798b017e29
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-09-10 08:45:09 +0500

    Data Destructuring

--- WebDevelopment - 1/JavaScript/Week Thirteen/Class 23/app.js
+++ WebDevelopment - 1/JavaScript/Week Thirteen/Class 23/app.js
@@ -1,1 +1,21 @@
 alert("Hello Students");
+
+// Data Destructuring (Arrays)
+
+function calculate(a, b) {
+    const add = a + b;
+    const subtract = a - b;
+    const multiply = a * b;
+    const divide = a / b;
+  
+    return [add, subtract, multiply, divide];
+  }
+  
+  const [add, subtract, multiply, divide] = calculate(4, 7);
+  
+  document.write("<p>Addition: " + add + "</p>");
+  document.write("<p>Subtraction " + subtract + "</p>");
+  document.write("<p>Multiplication: " + multiply + "</p>");
+  document.write("<p>Division " + divide + "</p>");
+
+  
