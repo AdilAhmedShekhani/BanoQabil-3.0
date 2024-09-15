19e5ad55d83946882bdc0ccf07669fc92e809199
AdilAhmedShekhani <adilaziz2009@live.com>
2024-09-15 15:23:36 +0500

    StopWatch

---
+++ WebDevelopment - 1/JavaScript/Week Thirteen/Stop Watch/app.js
@@ -0,0 +1,89 @@
+var msecCounter = 0;
+var secCounter = 0;
+var minCounter = 0;
+
+var min = document.getElementById("min");
+var sec = document.getElementById("sec");
+var msec = document.getElementById("msec");
+var counter = 0;
+var interval;
+
+function timer() {
+  msecCounter++;
+  msec.innerHTML = msecCounter;
+  console.log(msecCounter);
+  if (msecCounter === 100) {
+    secCounter++;
+    sec.innerHTML = secCounter;
+    msecCounter = 0;
+    if (secCounter === 5) {
+      secCounter = 0;
+      minCounter++;
+      min.innerHTML = minCounter;
+    }
+  }
+}
+
+function startTimer(e) {
+  // e.disabled = true
+  interval = setInterval(timer, 10);
+}
+
+function stopTimer() {
+  clearInterval(interval);
+}
+
+function resetTimer() {
+  clearInterval(interval);
+
+  min.innerHTML = "00";
+  sec.innerHTML = "00";
+  msec.innerHTML = "00";
+  secCounter = 0;
+  minCounter = 0;
+  msecCounter = 0;
+}var msecCounter = 0;
+var secCounter = 0;
+var minCounter = 0;
+
+var min = document.getElementById("min");
+var sec = document.getElementById("sec");
+var msec = document.getElementById("msec");
+var counter = 0;
+var interval;
+
+function timer() {
+  msecCounter++;
+  msec.innerHTML = msecCounter;
+  console.log(msecCounter);
+  if (msecCounter === 100) {
+    secCounter++;
+    sec.innerHTML = secCounter;
+    msecCounter = 0;
+    if (secCounter === 5) {
+      secCounter = 0;
+      minCounter++;
+      min.innerHTML = minCounter;
+    }
+  }
+}
+
+function startTimer(e) {
+  // e.disabled = true
+  interval = setInterval(timer, 10);
+}
+
+function stopTimer() {
+  clearInterval(interval);
+}
+
+function resetTimer() {
+  clearInterval(interval);
+
+  min.innerHTML = "00";
+  sec.innerHTML = "00";
+  msec.innerHTML = "00";
+  secCounter = 0;
+  minCounter = 0;
+  msecCounter = 0;
+}
