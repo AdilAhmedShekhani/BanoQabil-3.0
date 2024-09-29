523608038ebbb5feb5297b462a380fd37c6286f9
AdilAhmedShekhani <adilaziz2009@live.com>
2024-09-29 14:57:32 +0500

    Page Routing

--- WebDevelopment - 2/Next JS/Class 03/my-app/app/home/page.js
+++ WebDevelopment - 2/Next JS/Class 03/my-app/app/home/page.js
@@ -1,10 +1,23 @@
+"use client"
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 
 export default function Home() {
+  const router = useRouter()
+  const login = () => {
+    router.push("/shop")
+  }
   return (
+
     <div>
       <h1>
         Home
       </h1>
+      <Link href={"/shop"}>shop</Link>
+      <br/>
+      <button onClick={login}>login</button>
     </div>
   );
 }
---
+++ WebDevelopment - 2/Next JS/Class 03/my-app/app/shop/page.js
@@ -0,0 +1,15 @@
+"use client"
+
+
+export default function Shop() {
+
+  return (
+
+    <div>
+      <h1>
+        Shop
+      </h1>
+
+    </div>
+  );
+}
